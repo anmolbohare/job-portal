823a37cd57fd64b871bb7d71d8cbaaebe1cc31ae
anmolbohare <anmolbohare01@gmail.com>
2026-06-30 23:52:08 +0530

    Fix Clerk authentication for user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,20 +1,34 @@
 import express from 'express'
 import { applyForJob, getUserData, getUserJobApplications, updateUserResume } from '../controllers/userController.js'
 import upload from '../config/multer.js'
-
+// cg 
+import { requireAuth } from '@clerk/express'
 
 const router = express.Router()
 
-// Get user Data
-router.get('/user', getUserData)
+// // Get user Data
+// router.get('/user', getUserData)
 
-// Apply for a job
-router.post('/apply', applyForJob)
+// // Apply for a job
+// router.post('/apply', applyForJob)
 
-// Get applied jobs data
-router.get('/applications', getUserJobApplications)
+// // Get applied jobs data
+// router.get('/applications', getUserJobApplications)
 
-// Update user profile (resume)
-router.post('/update-resume', upload.single('resume'), updateUserResume)
+// // Update user profile (resume)
+// router.post('/update-resume', upload.single('resume'), updateUserResume)
+
+// cg 
+router.get('/user', requireAuth(), getUserData)
+
+router.post('/apply', requireAuth(), applyForJob)
+
+router.get('/applications', requireAuth(), getUserJobApplications)
+
+router.post(
+  '/update-resume',
+  requireAuth(),
+  upload.single('resume'),
+  updateUserResume)
 
 export default router;
